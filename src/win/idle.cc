f2e3e9a737570332d1a3831b0439592aacd18752
bithavoc <im@bithavoc.io>
2017-11-17 13:11:34 -0500

    return windows idle in seconds

--- src/win/idle.cc
+++ src/win/idle.cc
@@ -3,10 +3,10 @@
 #include <windows.h>
 
 double desktop_idle_get_time(void) {
-	LASTINPUTINFO lif;
-	lif.cbSize = sizeof(lif);
-	if (!GetLastInputInfo(&lif)) return -1;
-	uint64_t tickCount = GetTickCount64();
-	uint32_t IdleTime = (uint32_t)((tickCount - (uint64_t)lif.dwTime));
-	return static_cast<double>(IdleTime);
+  LASTINPUTINFO lif;
+  lif.cbSize = sizeof(lif);
+  if (!GetLastInputInfo(&lif)) return -1;
+  uint64_t tickCount = GetTickCount64();
+  uint32_t IdleTime = (uint32_t)((tickCount - (uint64_t)lif.dwTime));
+  return static_cast<double>(IdleTime / 1000);
 }
