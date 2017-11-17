dfc3256f8bbdde4e98f0bc8ae87c44abb4879977
bithavoc <im@bithavoc.io>
2017-11-17 12:56:29 -0500

    print test idle

--- test/main_test.js
+++ test/main_test.js
@@ -4,6 +4,7 @@ var desktopIdle = require('../');
 vows.describe('getIdleTime()').addBatch({
   'return value': () => {
     var idle = desktopIdle.getIdleTime()  
+    console.log("test idle", idle);
     assert.ok(idle > 0, 'should return idle time');
   }
 }).run();
